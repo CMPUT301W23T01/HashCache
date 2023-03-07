05b0e246db3d4c1c3c85fbfd84cddce68af341ca
Ryan Cooper <ryan.dm.cooper@gmail.com>
2023-03-07 16:08:20 -0700

    Begun work on the name generator

---
+++ app/src/main/java/com/example/hashcache/models/Generator.java
@@ -0,0 +1,15 @@
+package com.example.hashcache.models;
+
+/**
+ * Class that provides functionality from converting QR codes to names. By the hash they provide
+ */
+public class Generator {
+   /**
+    * Takes in a QR code and outputs the string that relates to it
+    * @param qr
+    * @return
+    */
+   public static String generateName(ScannableCode qr) {
+
+   }
+}
