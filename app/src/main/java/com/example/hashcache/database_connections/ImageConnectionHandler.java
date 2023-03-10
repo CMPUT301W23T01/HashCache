8db42f367132cb81e4eec3b01927c7c7e9127f41
Ryan Cooper <ryan.dm.cooper@gmail.com>
2023-03-09 18:57:21 -0700

    Added outline

---
+++ app/src/main/java/com/example/hashcache/database_connections/ImageConnectionHandler.java
@@ -0,0 +1,18 @@
+package com.example.hashcache.database_connections;
+
+public class ImageConnectionHandler {
+    /**
+     * Adds an image to the database. With some sort of reference to the document
+     */
+    public static void addImage() {
+
+    }
+
+    /**
+     * Gets an image from the database. Based on some reference
+     */
+    public static void getImage() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/hashcache/database_connections/converters/ImageDocumentConverter.java
@@ -0,0 +1,12 @@
+package com.example.hashcache.database_connections.converters;
+
+import static io.grpc.Context.LazyStorage.storage;
+
+public class ImageDocumentConverter {
+    /**
+     * Gets an image from the database
+     */
+    public void getImage() {
+        FireBaseSt
+    }
+}
