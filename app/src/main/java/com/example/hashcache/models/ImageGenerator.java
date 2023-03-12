37026a319d6f156473289952ccf82e2332b6a972
Syed Tasdid Azam Dhrubo <69814102+Dhrubo369@users.noreply.github.com>
2023-03-11 22:38:15 -0700

    Image Generator working version

--- app/src/main/java/com/example/hashcache/models/ImageGenerator.java
+++ app/src/main/java/com/example/hashcache/models/ImageGenerator.java
@@ -13,7 +13,7 @@ import java.io.InputStream;
 
 public class ImageGenerator {
 
-    public static Bitmap generateImage(Resources resources, int boxWidth, int boxHeight) {
+    public static Bitmap generateImage(Resources resources, int boxWidth, int boxHeight, int headX, int headY, int eyesX, int eyesY, int bodyX, int bodyY, int mouthX, int mouthY) {
         // example 64-bit binary string
         long binaryString = 0b1101100101010110001001101111101110010100100111110101101100001000L;
 
@@ -39,50 +39,27 @@ public class ImageGenerator {
 
         // load the image files for different parts
         Bitmap headImg = BitmapFactory.decodeResource(resources, getDrawableResourceId(resources, "head"));
-        if (headImg == null) {
-            Log.e("ImageGenerator", "headImg is null");
-        }
         Bitmap eyesImg = BitmapFactory.decodeResource(resources, getDrawableResourceId(resources, "eyes"+2));
-        if (eyesImg == null) {
-            Log.e("ImageGenerator", "eyesImg is null");
-        }
         Bitmap bodyImg = BitmapFactory.decodeResource(resources, getDrawableResourceId(resources, "body"+2));
-        if (bodyImg == null) {
-            Log.e("ImageGenerator", "bodyImg is null");
-        }
         Bitmap mouthImg = BitmapFactory.decodeResource(resources, getDrawableResourceId(resources, "mouth"+2));
-        if (mouthImg == null) {
-            Log.e("ImageGenerator", "mouthImg is null");
-        }
-
-        // resize the bitmaps to reduce their size
-        int width = boxWidth / 5; // set the width to 1/5 of the box width
-        headImg = Bitmap.createScaledBitmap(headImg, width, width, false);
-        eyesImg = Bitmap.createScaledBitmap(eyesImg, width, width, false);
-        bodyImg = Bitmap.createScaledBitmap(bodyImg, width, width, false);
-        mouthImg = Bitmap.createScaledBitmap(mouthImg, width, width, false);
 
         // create a new image of appropriate size
-        int height = headImg.getHeight() + eyesImg.getHeight() + bodyImg.getHeight();
         Bitmap image = Bitmap.createBitmap(boxWidth, boxHeight, Bitmap.Config.ARGB_8888);
 
         // draw different parts onto the final image
         Canvas canvas = new Canvas(image);
-        int x = (boxWidth - width) / 2; // center the head image horizontally
-        int y = 0;
-        canvas.drawBitmap(headImg, x, y, null);
 
-        x = (boxWidth - width) / 2; // center the eyes image horizontally
-        y = headImg.getHeight() - width / 2;
-        canvas.drawBitmap(eyesImg, x, y, null);
+        // draw head
+        canvas.drawBitmap(headImg, headX, headY, null);
 
-        x = (boxWidth - width) / 2; // center the body image horizontally
-        y = headImg.getHeight() + eyesImg.getHeight();
-        canvas.drawBitmap(bodyImg, x, y, null);
+        // draw eyes
+        canvas.drawBitmap(eyesImg, eyesX, eyesY, null);
 
-        x = (boxWidth - 2 * width) / 2; // center the mouth image horizontally
-        y = headImg.getHeight() + eyesImg.getHeight() + bodyImg.getHeight();
-        canvas.drawBitmap(mouthImg, x, y, null);
+        // draw body
+        canvas.drawBitmap(bodyImg, bodyX, bodyY, null);
+
+        // draw mouth
+        canvas.drawBitmap(mouthImg, mouthX, mouthY, null);
 
         return image;
     }
@@ -90,6 +67,5 @@ public class ImageGenerator {
     private static int getDrawableResourceId(Resources resources, String name) {
         return resources.getIdentifier(name, "drawable", "com.example.hashcache");
     }
-
 
 }
